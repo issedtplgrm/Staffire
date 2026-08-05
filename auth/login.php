f4121ac8312b1491f46a683936a441eda4456cf3
issedtplgrm <issedtpilgrim@gmail.com>
2026-08-05 13:47:19 +0800

    login fields

--- auth/login.php
+++ auth/login.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="../assets/css/login.css">
 </head>
+
 <body>
-    <div class="login-cont">
-        <label for="email">Email</label>
-            <input type="text" id="password" name="password" placeholder="example@gmail.com">
-        <label for="password">Password</label>
-            <input type="password" id="password" name="password">
-    </div>
+    <main class="card">
+        <form action="">
+            <div class="field-group">
+                <input type="text" id="email" placeholder=" " required>
+                <label for="name">Email</label>
+            </div>
+
+            <div class="field-group">
+                <input type="password" id="password" placeholder=" " required>
+                <label for="password">Password</label>
+            </div>
+        </form>
+    </main>
 </body>
+
 </html>
