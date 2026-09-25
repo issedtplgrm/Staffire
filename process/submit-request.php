38b6b6f6caffe3b723f69318d883aeb1d48a4f06
Moliren-cmd <christianjadechavez@gmail.com>
2026-09-26 00:39:41 +0800

    finalized

--- process/submit-request.php
+++ process/submit-request.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 $userId = (int) $_SESSION['id'];
 $userRole = $_SESSION['role'];
 $requestType = $_POST['request_type'] ?? '';
-$redirect = $userRole === 'manager' ? '../pages/request-leave.php' : '../pages/empDashboard.php';
+$redirect = $userRole === 'manager' ? '../pages/dashboard.php' : '../pages/dashboard.php';
 
 try {
     $db = new Database();
